b2eea746262c1e6bd4ae9c6bfac92030722985b9
Lakshay <lakshaysehrawat2111@gmail.com>
2025-07-25 15:46:53 +0530

    chatapp upload

---
+++ server/db/connection1.db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const MONGO_URI = process.env.MONGO_URI;
+
+    if (!MONGO_URI) {
+      throw new Error("❌ MONGO_URI not found in .env");
+    }
+
+    const instance = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${instance.connection.host}`);
+    console.log("Connected to DB:", mongoose.connection.name);
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1);
+  }
+};
